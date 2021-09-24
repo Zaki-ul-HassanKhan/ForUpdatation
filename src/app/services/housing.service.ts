5f97dabe4da99b9c62ff3ff9053af3343a795e57
Zaki <zakiulhassankhan@gmail.com>
2021-09-24 18:20:23 +0500

    third-comit

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { IProperty } from '../property/IProperty.interface';
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
 
+
   constructor(private http:HttpClient) { }
-  getAllProperties(){
-    return this.http.get('data/properties.json')
+  getAllProperties(SellRent: number): Observable<IProperty[]>{
+    return this.http.get('data/properties.json').pipe(
+      map(data => {
+        const propertiesArray: Array<any> =[];
+       for (const id in data){
+          if (data.hasOwnProperty(id)){
+             propertiesArray.push(data[id]);
+          }
+       }
+      return propertiesArray;
+      }
+
+      )
+    )
 
   }
 }
